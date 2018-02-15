bb7028dfd64bde33938d4847204d0d7bdaaf3c70
depdroid <iguslee@gmail.com>
2018-02-15 15:50:55 +0700

    commit again

--- app/src/main/java/com/dwiprastya/kotlinrecycleview/MainActivity.kt
+++ app/src/main/java/com/dwiprastya/kotlinrecycleview/MainActivity.kt
@@ -2,11 +2,25 @@ package com.dwiprastya.kotlinrecycleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rv = findViewById<RecyclerView>(R.id.recyclerView1)
+        rv.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL,false)
+        val users = ArrayList<User>()
+        users.add(User("Paul","Mr"))
+        users.add(User("Jane","Miss"))
+        users.add(User("John", "Dr"))
+        users.add(User("Amy", "Mrs"))
+
+        val adapter = CustomAdapter(users)
+        rv.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/dwiprastya/kotlinrecycleview/User.kt
@@ -0,0 +1,6 @@
+package com.dwiprastya.kotlinrecycleview
+
+/**
+ * @author : dpdroid
+ */
+data class User(val name:String,val title:String)
